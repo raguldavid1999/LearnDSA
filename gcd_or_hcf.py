6f154c2351be3cc8f8a1f04afe584aa3fd7bce0f
Ragul David <raguldavid1999@gmail.com>
2023-10-23 15:04:35 +0530

    Find GCD or HCF

---
+++ gcd_or_hcf.py
@@ -0,0 +1,36 @@
+# def gcd(a, b):
+#     min = 0
+#     if(a>b):
+#         min = b 
+#     else:
+#         min = a
+
+#     for i in range(min,0,-1):
+#         if(a%i==0 and b%i==0):
+#             return i
+#     return 1
+
+# def eqlid_gcd(a, b):
+#     while(a!=b):
+#         if(a>b):
+#             a = a-b
+#         else: 
+#             b = b-a
+#     return a
+
+def gabriel_gcd(a, b):
+    while(a!=0 and b!=0):
+        if(a>b):
+            a = a%b
+        else: 
+            b = b%a
+    if(a!=0):
+        return a
+    else:
+        return b
+        
+a = 20
+b = 15
+# print(gcd(a,b))
+# print(eqlid_gcd(a,b))
+print(gabriel_gcd(a,b))
