d7736c30f23ca10968ea6b22786056b7038b7a11
Ragul David <raguldavid1999@gmail.com>
2023-10-27 23:09:43 +0530

    efficient way to find right most set bit

--- right_most_bit.py
+++ right_most_bit.py
@@ -1,12 +1,16 @@
-def posOfrmsb(n):
-    m = 1
-    position = 0
-    if n==0:
-        return -1
-    while((n & m)==0):
-        m = m << 1
-        position = position + 1
-    return position + 1
+# def posOfrmsb(n):
+#     m = 1
+#     position = 0
+#     if n==0:
+#         return -1
+#     while((n & m)==0):
+#         m = m << 1
+#         position = position + 1
+#     return position + 1
 
-print(posOfrmsb(256))
+import math
+def posOfrmsb(n):
+    return int(math.log10(n ^ (n & (n-1))) / math.log10(2)) + 1
+
+print(posOfrmsb(40))
 
